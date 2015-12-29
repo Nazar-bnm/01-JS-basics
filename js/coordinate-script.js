a784588028da2ed70a835ab5977d06b3f2b7cc2e
Nazar-bnm <nazar.bnm@gmail.com>
2015-12-29 13:31:21 +0200

    added check isNan to the file game-jscript.js

--- js/coordinate-script.js
+++ js/coordinate-script.js
@@ -4,7 +4,7 @@
         y1 = -2;
     var x2 = prompt("Please, enter coordinate x", 0);
     var y2 = prompt("Please, enter coordinate y", 0);
-    var distance = Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
+    var distance = Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
     var sentenceDistance = "The distance between points A(" + x1 + "," + y1 + ") and B(" + x2 + "," + y2 + ") is " + distance;
     alert(sentenceDistance);
 })();
